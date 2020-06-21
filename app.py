737aa7f881159700c491dcd65c460eca43df48c4
chanakarn4726 <chanakarn4726@gmail.com>
2020-06-22 01:57:31 +0700

    update

--- app.py
+++ app.py
@@ -41,12 +41,12 @@ def blog():
 def singleblog():
     return render_template('single-blog.html')
 
-@app.route("/login", methods=[POST])
+@app.route("/login", methods=[GET,POST])
 def login():
-    if request.method == 'POST':
-        return '<h1>error</h1>'
-    else:
+    if request.method == 'GET':
         return render_template('login.html')
+    else:
+        return '<h1>error</h1>'
 
 @app.route("/checkauth ", methods=['GET','POST'])
 def checkauth():
