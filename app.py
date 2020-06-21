8ba73b5d243cbb36f35eec7e8dda8c3e85f2fe48
chanakarn4726 <chanakarn4726@gmail.com>
2020-06-22 04:50:53 +0700

    update datebase

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///db.sqlit3'
+app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///db.sqlit'
 
 db.SQLAlchemy(app)
 
