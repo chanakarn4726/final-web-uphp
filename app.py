7424207ab9b3f270d13557fd4254472bda7c2284
chanakarn4726 <chanakarn4726@gmail.com>
2020-06-22 05:10:25 +0700

    'update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
-app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:////home/sshuser/final-web-uphp/data.db'
+app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite3:////home/sshuser/final-web-uphp/data.db'
 
 db.SQLAlchemy(app)
 
